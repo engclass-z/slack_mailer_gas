85ede16c03b8d49a1973166e3eee81624f30b871
takuya_inoue <t.inoue38@gmail.com>
2022-05-13 04:44:00 +0900

    add slack post logic

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import GmailMessage = GoogleAppsScript.Gmail.GmailMessage;
 
 const searchQuery = 'subject: 【slack_mailer_gas】'; // メール検索する際の検索クエリ
+const slackOAuthToken = 'xxxx';
+const channelId = 'xxxx';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const main = () => {
@@ -8,9 +10,23 @@ const main = () => {
   const threads = GmailApp.search(query, 0, 10);
   threads.forEach((thread) => {
     thread.getMessages().forEach((message) => {
+      postToSlack(message.getId(), message.getFrom(), message.getSubject(), message.getPlainBody()); // slack 送信
+
       markRead(message); // 処理済み
     });
   });
+};
+
+const postToSlack = (id: string, from: string, subject: string, body: string) => {
+  const slackApp = SlackApp.create(slackOAuthToken);
+  const message = `
+  id: ${id}
+  from: ${from}
+  subject: ${subject}
+  
+  body: 
+  ${body}`;
+  slackApp.postMessage(channelId, message);
 };
 
 /**
@@ -19,3 +35,16 @@ const main = () => {
 const markRead = (message: GmailMessage) => {
   message.markRead();
 };
+
+/**
+ * 以下 SlackApp の定義
+ */
+declare const SlackApp: SlackApp;
+
+interface SlackAppAPI {
+  postMessage(channelId: string, message: string): void;
+}
+
+interface SlackApp {
+  create(token: string): SlackAppAPI;
+}
