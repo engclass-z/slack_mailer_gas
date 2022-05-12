fe1bbc12e02a1aa602ffc51256ca740a9db569da
takuya_inoue <t.inoue38@gmail.com>
2022-05-13 03:33:14 +0900

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import GmailMessage = GoogleAppsScript.Gmail.GmailMessage;
-import DoPost = GoogleAppsScript.Events.DoPost;
 
 const searchQuery = 'subject: 【slack_mailer_gas】'; // メール検索する際の検索クエリ
 
@@ -19,12 +18,4 @@ const main = () => {
  */
 const markRead = (message: GmailMessage) => {
   message.markRead();
-};
-
-/**
- * post を受け取りそのまま返す API
- */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const doPost = (e: DoPost) => {
-  return ContentService.createTextOutput(e.postData.contents);
 };
